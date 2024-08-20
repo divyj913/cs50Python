9d3cd07f8253741c582fdee6c8920ac11cf1a6d0
DJ <DIVYJ913@GMAIL.COM>
2024-08-20 15:29:55 +0530

    Update meal.py

--- week1/meal.py
+++ week1/meal.py
@@ -1,8 +1,17 @@
 def main():
-    prompt = input("What's the time ?")
+    prompt = input("What time is it? ")
     prompt = convert(prompt)
-    
+    if 7<= prompt <= 8:
+        print("breakfast time")
+    elif 12<= prompt <= 13:
+        print("Lunch time ")
+    elif 18<= prompt <= 19:
+        print("Dinner time ")
+    else:
+        print("eat nothing.")
 def convert(t):
-    if t.endswith('a.m.'):
-        t = t.split('a.m.')
-        
+    h , m = t.split(":")
+    h = float(h)
+    m = float(m)
+    return h+m/60
+main()
